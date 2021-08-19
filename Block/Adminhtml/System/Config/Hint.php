f393eb3548a59bcafb576051baf1973b8bc05a1e
vitaliy <vitaliy@moogento.com>
2021-08-19 11:12:14 +0300

    sitemap: added logo section on the config page

---
+++ Block/Adminhtml/System/Config/Hint.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Moogento\Sitemap\Block\Adminhtml\System\Config;
+
+use Magento\Backend\Block\Template;
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\Data\Form\Element\Renderer\RendererInterface;
+
+/**
+ * @method Hint setElement
+ */
+class Hint extends Template implements RendererInterface
+{
+    /**
+     * @var string
+     */
+    protected $_template = 'Moogento_Sitemap::system/config/hint.phtml';
+
+    /**
+     * @param Context $context
+     * @param array $data
+     */
+    public function __construct(Context $context, array $data = [])
+    {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Render form element as HTML
+     *
+     * @param AbstractElement $element
+     * @return string
+     */
+    public function render(AbstractElement $element): string
+    {
+        $this->setElement($element);
+        return $this->toHtml();
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogo(): string
+    {
+        return $this->_scopeConfig->getValue('moogento/logo/url')
+            . 'media/moo_logo/moogento_logo_sitemap.png';
+    }
+
+    /**
+     * @return string
+     */
+    public function getInfo(): string
+    {
+        return $this->_scopeConfig->getValue('moogento/logo/url')
+            . 'media/moo_info/moogento_sitemap.js';
+    }
+}
